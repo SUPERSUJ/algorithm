073e51e2822ddf6714c518dd72395a71b790599a
SUPERSUJ <18124610289@163.com>
2018-10-31 23:01:33 +0800

    修改

--- util.js
+++ util.js
@@ -1,28 +1,36 @@
-var KEY = 'm2f1Oh2NB7Az98pUdmtCIo83MeZ7gRV4';
-var util = {
-  sortData: function(obj) {
-    var keys = [];
-    var objStr = '';
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        keys.push(key);
-      }
+
+// key 排序拼接
+function sortKey (obj) {
+  var keys = [];
+  var objStr = '';
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      keys.push(key);
     }
-    keys = keys.sort(function(a, b) {
-      return a < b ? -1 : 1;
-    });
-    for (var i = 0, keysLen = keys.length; i < keysLen; i++) {
-      var key = keys[i];
-      objStr += key + '=' + obj[key] + '&&';
-    }
-    objStr = objStr + 'key=' + KEY;
-    return objStr;
-  },
-  isNumber: function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-  },
-};
-var countDecimals = function () {
-  if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
-  return this.toString().split(".")[1].length || 0; 
+  }
+  var keysLen = keys.length;
+  keys = keys.sort(function (a, b) {
+    return a < b ? -1 : 1;
+  });
+  if (keysLen > 0) {
+    objStr += keys[0] + '=' + obj[keys[0]];
+  }
+  for (var i = 1; i < keysLen; i++) {
+    var key = keys[i];
+    objStr = objStr + '&&' + key + '=' + obj[key];
+  }
+  return objStr;
+}
+
+// 是否是数字
+function isNumber (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// 判断小数点后几位
+function countDecimals(n) {
+  if (Math.floor(n) === n.valueOf()) {
+    return 0
+  };
+  return n.toString().split(".")[1].length || 0;
 }
